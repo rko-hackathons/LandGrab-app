d87a9de72fe57b204ec6079ada60457026e0a54a
mkobar <mike@exlorev.com>
2016-06-10 18:44:44 -0400

    from landgrab-static built from creator

---
+++ www/js/controllers.js
@@ -0,0 +1,50 @@
+angular.module('app.controllers', [])
+  
+.controller('captureCtrl', function($scope) {
+
+})
+   
+.controller('cartTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+         
+.controller('splashCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('AreaNameCtrl', function($scope) {
+
+})
+   
+.controller('AreaName2Ctrl', function($scope) {
+
+})
+   
+.controller('setQuestionForAreaNameCtrl', function($scope) {
+
+})
+   
+.controller('earnedTipsCtrl', function($scope) {
+
+})
+   
+.controller('leaderboardCtrl', function($scope) {
+
+})
+   
+.controller('profileCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,123 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.capture', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/capture.html',
+        controller: 'captureCtrl'
+      }
+    }
+  })
+
+  .state('cartTabDefaultPage', {
+    url: '/page3',
+    templateUrl: 'templates/cartTabDefaultPage.html',
+    controller: 'cartTabDefaultPageCtrl'
+  })
+
+  .state('cloudTabDefaultPage', {
+    url: '/page4',
+    templateUrl: 'templates/cloudTabDefaultPage.html',
+    controller: 'cloudTabDefaultPageCtrl'
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('splash', {
+    url: '/page5',
+    templateUrl: 'templates/splash.html',
+    controller: 'splashCtrl'
+  })
+
+  .state('login', {
+    url: '/page6',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/page7',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('tabsController.AreaName', {
+    url: '/page8',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/AreaName.html',
+        controller: 'AreaNameCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.AreaName2', {
+    url: '/page9',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/AreaName2.html',
+        controller: 'AreaName2Ctrl'
+      }
+    }
+  })
+
+  .state('tabsController.setQuestionForAreaName', {
+    url: '/page10',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/setQuestionForAreaName.html',
+        controller: 'setQuestionForAreaNameCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.earnedTips', {
+    url: '/page11',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/earnedTips.html',
+        controller: 'earnedTipsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.leaderboard', {
+    url: '/page12',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/leaderboard.html',
+        controller: 'leaderboardCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.profile', {
+    url: '/page13',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/profile.html',
+        controller: 'profileCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/page1/page2')
+
+  
+
+});
